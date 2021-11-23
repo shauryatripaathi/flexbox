dfde3ac87474e31f7459003d8b5afe13b3e42afe
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2021-11-23 16:59:26 +0530

    Update init.js

--- init.js
+++ init.js
@@ -1,5 +1,21 @@
 let box1 = document.getElementById("box1");
 box1.addEventListener("click", myfunc);
-function myfunc(){
-    box1.innerHTML = "x";
+function myfunc() {
+    if (box1.innerHTML === "") {
+        box1.innerHTML = "x";
+    }
+    else {
+        box1.innerHTML = "";
+    }
 }
+let box2 = document.getElementById("box2");
+box2.addEventListener("click", mysecfunc);
+function mysecfunc() {
+    if (box2.innerHTML === "") {
+        box2.innerHTML = "o";
+    }
+    else {
+        box2.innerHTML = "";
+    }
+}
+
